74dbc7ada09abb788ff7723734b6c2611a45d17f
MolinDeng <zjumorning@gmail.com>
2024-10-28 13:54:59 -0400

    add blue noise and condig steps

--- src/app/test3/page.tsx
+++ src/app/test3/page.tsx
@@ -2,36 +2,49 @@
 
 import { Suspense, useRef } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import vertexShader from '@/shaders/test3/vertexShader.glsl';
-import fragmentShader from '@/shaders/test3/fragmentShader.glsl';
 // import { useWindowSize } from '@/hooks/UseWindowSize';
 import { useTexture } from '@react-three/drei';
 import * as THREE from 'three';
+import { Leva, useControls } from 'leva';
 
-const DPR = 1;
+import vertexShader from '@/shaders/test3/vertexShader.glsl';
+import fragmentShader from '@/shaders/test3/fragmentShader.glsl';
+
 // Noise Texture
 const NOISE_TEXTURE_URL = 'https://cdn.maximeheckel.com/noises/noise2.png';
+// Blue noise texture
+const BLUE_NOISE_TEXTURE_URL = 'https://cdn.maximeheckel.com/noises/blue-noise.png';
 
-const Raymarching = () => {
+const Raymarching = ({dpr, maxSteps, marchSize}: {dpr: number, maxSteps: number, marchSize: number}) => {
   const { viewport } = useThree();
-  const noisetexture = useTexture(NOISE_TEXTURE_URL, (tex) => {
-    tex.wrapS = THREE.RepeatWrapping;
-    tex.wrapT = THREE.RepeatWrapping;
-    tex.minFilter = THREE.NearestMipmapLinearFilter;
-    tex.magFilter = THREE.LinearFilter;
-  });
+  const [noisetexture, blueNoiseTexture] = useTexture(
+    [NOISE_TEXTURE_URL, BLUE_NOISE_TEXTURE_URL],
+    (textures) => {
+      textures[0].wrapS = textures[1].wrapS = THREE.RepeatWrapping;
+      textures[0].wrapT = textures[1].wrapT = THREE.RepeatWrapping;
+      textures[0].minFilter = textures[1].minFilter = THREE.NearestMipmapLinearFilter;
+      textures[0].magFilter = textures[1].magFilter = THREE.LinearFilter;
+    }
+  );
 
   const uniforms = useRef({
     uTime: { value: 0 },
     uResolution: {
-      value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+      value: new THREE.Vector2(window.innerWidth * dpr, window.innerHeight * dpr),
     },
     uNoise: { value: noisetexture },
+    uBlueNoise: { value: blueNoiseTexture },
+    uFrame: { value: 0 },
+    uMaxSteps: { value: maxSteps },
+    uMarchSize: { value: marchSize },
   }).current;
 
   useFrame((state, delta) => {
     uniforms.uTime.value += delta;
-    uniforms.uResolution.value.set(window.innerWidth * DPR, window.innerHeight * DPR);
+    uniforms.uResolution.value.set(window.innerWidth * dpr, window.innerHeight * dpr);
+    uniforms.uFrame.value += 1;
+    uniforms.uMaxSteps.value = maxSteps;
+    uniforms.uMarchSize.value = marchSize;
   });
 
   return (
@@ -47,22 +60,30 @@ const Raymarching = () => {
 };
 
 const Scene = () => {
+  const {dpr, maxSteps, marchSize} = useControls({
+    dpr: { value: 0.5, min: 0.1, max: 2, step: 0.1 },
+    maxSteps: { value: 40, min: 10, max: 200, step: 10 },
+    marchSize: { value: 0.16, min: 0.10, max: 0.30, step: 0.01 },
+  });
   return (
-    <Canvas
-      camera={{ position: [0, 0, 6] }}
-      dpr={DPR}
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
-      <Suspense fallback={null}>
-        <Raymarching />
-      </Suspense>
-    </Canvas>
+    <>
+      <Canvas
+        camera={{ position: [0, 0, 6] }}
+        dpr={dpr}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <Suspense fallback={null}>
+          <Raymarching dpr={dpr} maxSteps={maxSteps} marchSize={marchSize} />
+        </Suspense>
+      </Canvas>
+      <Leva />
+    </>
   );
 };
 
--- src/shaders/test3/fragmentShader.glsl
+++ src/shaders/test3/fragmentShader.glsl
@@ -1,8 +1,12 @@
 uniform float uTime;
 uniform vec2 uResolution;
 uniform sampler2D uNoise;
+uniform sampler2D uBlueNoise;
+uniform int uFrame;
 
-#define MAX_STEPS 100
+uniform int uMaxSteps;
+uniform float uMarchSize;
+
 #define PI 3.14159265359
 
 mat2 rotate2D(float a) {
@@ -73,31 +77,31 @@ float scene(vec3 p) {
 }
 
 const vec3 SUN_POSITION = vec3(1.0, 0.0, 0.0);
-const float MARCH_SIZE = 0.08;
 
-vec4 raymarch(vec3 rayOrigin, vec3 rayDirection) {
+vec4 raymarch(vec3 rayOrigin, vec3 rayDirection, float offset) {
   float depth = 0.0;
+  depth += uMarchSize * offset;
   vec3 p = rayOrigin + depth * rayDirection;
   vec3 sunDirection = normalize(SUN_POSITION);
 
   vec4 res = vec4(0.0);
 
-  for (int i = 0; i < MAX_STEPS; i++) {
+  for (int i = 0; i < uMaxSteps; i++) {
     float density = scene(p);
 
     // We only draw the density if it's greater than 0
     if (density > 0.0) {
       // Directional derivative
       // For fast diffuse lighting
-      float diffuse = clamp((scene(p) - scene(p + 0.3 * sunDirection)) / 0.3, 0.0, 1.0 );
+      float diffuse = clamp((scene(p) - scene(p + 0.3 * sunDirection))/0.3, 0.0, 1.0 );
       vec3 lin = vec3(0.60,0.60,0.75) * 1.1 + 0.8 * vec3(1.0,0.6,0.3) * diffuse;
-      vec4 color = vec4(mix(vec3(1.0, 1.0, 1.0), vec3(0.0, 0.0, 0.0), density), density );
+      vec4 color = vec4(mix(vec3(1.0,1.0,1.0), vec3(0.0, 0.0, 0.0), density), density );
       color.rgb *= lin;
       color.rgb *= color.a;
-      res += color * (1.0 - res.a);
+      res += color*(1.0-res.a);
     }
 
-    depth += MARCH_SIZE;
+    depth += uMarchSize;
     p = rayOrigin + depth * rayDirection;
   }
 
@@ -126,8 +130,11 @@ void main() {
   // Add sun color to sky
   color += 0.5 * vec3(1.0,0.5,0.3) * pow(sun, 10.0);
 
+  float blueNoise = texture2D(uBlueNoise, gl_FragCoord.xy / 1024.0).r;
+  float offset = fract(blueNoise + float(uFrame%32) / sqrt(0.5));
+
   // Cloud
-  vec4 res = raymarch(ro, rd);
+  vec4 res = raymarch(ro, rd, offset);
   color = color * (1.0 - res.a) + res.rgb;
   gl_FragColor = vec4(color, 1.0);
 }
